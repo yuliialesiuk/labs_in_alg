cd128279235acca096df1ce6acb5b65ae727ec9e
pomy <121321457+yuliialesiuk@users.noreply.github.com>
2023-02-01 13:02:41 +0200

    Update lab8.cpp

--- lab8.cpp
+++ lab8.cpp
@@ -1,24 +1,46 @@
-// lab8.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include<fstream>
-#pragma warning(disable:4996)
+#include <fstream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 int main()
 {
-    FILE* fptr;
-    string line;
-    fptr = fopen("file_for_lab8", "w+");
+    ofstream foptr;
+    ifstream fiptr;
 
-    if (fptr == NULL) {
+    string line;
+    foptr.open("file_for_lab8");
+    fiptr.open("file_for_getting_lab8");
+
+    if (!foptr) {
+        cout << "Error! File dose not exist!" << endl;
+        return 0;
+    }
+    if (!fiptr) {
         cout << "Error! File dose not exist!" << endl;
         return 0;
     }
     
-    getline(fin, line);
+    getline(fiptr, line);
 
-    fclose(fptr);
+    int len = line.length();
+
+    for (int i = 0; i < len; i++) {
+        if (line[i] = 1) {
+            line[i] = 0;
+        }
+        else if (line[i] = 0) {
+            line[i] = 1;
+        }
+
+    }
+
+    foptr << line << endl;
+
+    foptr.close();
+    fiptr.close();
     return 0;
 }
+
