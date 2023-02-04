581a0d4c24757bd3f1783de6d3c8a8044ef8d42a
pomy <121321457+yuliialesiuk@users.noreply.github.com>
2023-02-04 18:33:30 +0200

    lab6

--- lab6.cpp
+++ lab6.cpp
@@ -1,23 +1,61 @@
+
+//Ввести стрічку довільної довжини, яку трактуватимемо як текст, у якому
+//слова відокремлені одним або кількома інтервалами(довжина слів,
+//    загалом, довільна).Вважаючи, що текст може містити слова, які
+//    складаються як з букв, так із цифр, а також слова, у які цифри не входять,
+//    вивести у вигляді стрічки всі слова, що не містять цифр.Слова у стрічці
+//    відокремити трьома інтервалами.
+
+//how to call a word to find words with num?
+
 #include <iostream>  
 #include <cstring>  
+#include<ctype.h>
 using namespace std;
+
+bool analise_word(char*);
 
 int main()
 {
-    char str[100];   
+    const int s_len = 100;
+    char str[s_len+1];   
     cout << " Enter a string: " << endl;
-    cin.getline(str, 100); 
-
+    cin.getline(str, s_len); 
+    char rsd[] = " .,";
     char* ptr; 
-    ptr = strtok(str, " "); 
+    ptr = strtok(str, rsd); 
+
+    int flag = 0;
+    int flag_d = 1;
+
     while (ptr != NULL)
     {
-        cout << ptr << endl;
-        ptr = strtok(NULL, " ");
+        flag = 1;
+        if (analise_word(ptr)) {
+            cout << ptr << endl;
+            flag_d = 0;
+        }
+        ptr = strtok(NULL, rsd);
     }
+    if (!flag) {
+        cout << "text is enpty\n";
+    }
+    else if (flag_d) {
+        cout << "All words are digital\n";
+    }
+    cout << "end of program\n";
+
 
 
 
     return 0;
 }
 
+bool analise_word(char* w)
+{
+    for (int i = 0; w[i]; i++) {
+        if (isdigit(w[i]))
+            return false;
+    }
+    return true;
+}
